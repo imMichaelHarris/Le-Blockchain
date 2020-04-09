1da1dbf81305d172efd378e23a507b1b6f06c8cd
Michael Harris <mhguitar1997@gmail.com>
2020-04-09 13:19:13 -0700

    Import json, sys, and requests

--- basic_wallet_p/wallet.py
+++ basic_wallet_p/wallet.py
@@ -1,5 +1,9 @@
+import requests
+import sys
+import json
 
 print("Blockchain wallet")
 name = input("Enter your name: ")
+r = requests.post("http://localhost:5000/last_block")
 
 print(f"Welcome {name}! Checking your balance now...")
